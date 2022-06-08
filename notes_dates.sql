55e6bd5ca9a88ade3ef94a623cd71a83f90baa08
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-08 18:38:01 +0200

    Create notes_dates.sql

---
+++ notes_dates.sql
@@ -0,0 +1,17 @@
+FUNCTIONS
+############################################################
+
+yy, yyyy  YEAR()
+qq, q     QUARTER()
+mm, m     MONTH()
+dd, d     DAY()
+          DAYOFMONTH()
+dw        DAYOFWEEK()  /  WEEKDAY()
+dy, y     DAYOFYEAR()
+wk, ww    WEEK()
+hh        HOUR()
+mi, n     MINUTE()
+ss, s     SECOND() 
+ms        MILISECOND()
+mcs       MICROSECOND()
+ns        NANOSECOND()
