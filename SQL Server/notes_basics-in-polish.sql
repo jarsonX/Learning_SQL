fe7af49450d898759b8a58b291f9b8cb510a6b41
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-09 13:39:54 +0200

    Update notes_basics-in-polish.sql

--- SQL Server/notes_basics-in-polish.sql
+++ SQL Server/notes_basics-in-polish.sql
@@ -2,11 +2,13 @@ Podstawy SQL w wersji SQL Server
 
 _________________________________________________________________________________________________________________
 SKŁADNIA PODSTAWOWA
-
-SELECT kolumna_1, kolumna_2     atrybuty
-FROM tabela                     relacje
-WHERE wyrażenie_logiczne        ograniczenia
-ORDER BY kolumna_1, kolumna_2
+                                  Kolejność wg której działa silnik
+SELECT kolumna_1, kolumna_2       5
+FROM tabela                       1
+WHERE wyrażenie_logiczne          2
+GROUP BY kryterium grupowania     3 
+HAVING kryterium filtrowania      4
+ORDER BY kolumna_1, kolumna_2     6
 
 _________________________________________________________________________________________________________________
 FUNKCJE AGREGUJĄCE
@@ -20,3 +22,23 @@ FROM tabela
 GROUP BY xxx
 HAVING yyy > 1
 
+WHERE filtruje wiersze, HAVING filtruje grupy.
+W klauzulach GROUP BY i HAVING nie używamy aliasów.
+
+_________________________________________________________________________________________________________________
+ŁĄCZENIE TABEL
+
+Iloczyn kartezjański - zbiór, w którym każdy wiersz z pierwszej tabeli połączony jest z każdym wierszem
+w drugiej tabeli. Wiersze niepasujące do złączenia są odrzucane. Istotne są klucze główne i obce.
+
+>>> ZŁĄCZENIA WEWNĘTRZNE
+
+JOIN... ON...
+INNER JOIN... ON...
+
+>>> ZŁĄCZENIA ZEWNĘTRZNE
+
+LEFT JOIN... ON...    wiersze z lewej, dla których nie ma odpowiedników z prawej
+RIGHT JOIN... ON...
+FULL JOIN... ON...
+CROSS JOIN... ON...
