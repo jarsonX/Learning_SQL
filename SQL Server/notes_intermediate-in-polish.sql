27d6dda8124b75e42f2a87c4fba064fbdd47947a
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-09 17:45:57 +0200

    Update notes_intermediate-in-polish.sql

--- SQL Server/notes_intermediate-in-polish.sql
+++ SQL Server/notes_intermediate-in-polish.sql
@@ -1,3 +1,36 @@
 SQL w wersji SQL Server
 
 _________________________________________________________________________________________________________________
+CAST
+
+CAST(expression AS data_type [(length)]) - konwertuje zmienną na inny typ
+
+_________________________________________________________________________________________________________________
+PRZYDATNE FUNKCJE
+
+LEN (string_expression)
+LEFT/RIGHT (text, length)
+SUBSTRING (text, start, length)
+LTRIM (text) - czyści białe znaki
+REPLACE(text, pattern, replacement)
+CONCAT(text1, text2, textN)
+LOWER/UPPER(text)
+CHARINDEX(text_to_find, text_to_search, [start])
+
+________________________________________________________________________________________________________________
+ZMIENNE
+
+DECLARE - deklaracja zmiennej (nazwa, typ)
+SET - przypisanie wartości zmiennej
+
+Przykład:
+
+DECLARE @LastName VARCHAR(30),
+        @FirstName VARCHAR(30)
+        
+SET @LastName = 'Leon'
+SET @FirstName = 'Pies'
+
+SELECT LastName, FirstName, HomePhone
+FROM Employees
+WHERE LastName LIKE @LastName AND FirstName LIKE @FirstName
