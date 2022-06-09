20201bfb525a38863a4be8df3e75ba27005af4c4
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-09 18:27:38 +0200

    Update notes_tables-in-polish.sql

--- SQL Server/notes_tables-in-polish.sql
+++ SQL Server/notes_tables-in-polish.sql
@@ -18,6 +18,41 @@ UPDATE Production.Product
 SET Color = N'Metallic Red'
 WHERE Name LIKE N'Road-250%' AND Color = N'Red'
 
-3). UPDATE + TOP + WHERE
+3). UPDATE + TOP
 
 UPDATE TOP(10) HumarResources.Employee
+SET VacationHours = VacationHours * 1.25
+
+4). UPDATE + TOP + WHERE
+
+UPDATE HumanResources.Employee
+SET VacationHours = VacationHours + 8
+FROM (SELECT TOP(10) BusinessEntityID
+      FROM HumanResources.Employee
+      ORDER BY HireDate) AS th
+WHERE HumanResources.Employee.BusinessEntityID = th.BusinessEntityID      
+
+_________________________________________________________________________________________________________________
+INSERT INTO
+
+Dodanie nowego wiersza do tabeli.
+
+Przykład:
+
+INSERT INTO Employees (ID, fname, lname)
+  VALUES (3022, 'John', 'Smith')
+  
+INSERT INTO OrdersArchive (ID, date, ship_name)
+  SELECT ID, date, ship_name FROM Orders
+  WHERE order_date >= (DATE()-30)
+  
+_________________________________________________________________________________________________________________
+DELETE
+
+Usuwanie rekordów. 
+
+Dodanie nowego wiersza do tabeli.
+
+DELETE FROM Production.History    - usuwa wszystkie rekordy
+WHERE ...                         - opcjonalne, pozwala ograniczyć zakres
+
