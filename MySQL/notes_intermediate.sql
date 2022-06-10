edbc976bfdf9c4a6a0449715327754094994c4f9
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-10 13:06:02 +0200

    Update notes_intermediate.sql

--- MySQL/notes_intermediate.sql
+++ MySQL/notes_intermediate.sql
@@ -31,4 +31,29 @@ FROM (SELECT EMP_ID, F_NAME, L_NAME, DEP_ID
       FROM EMPLOYEES) AS EMP4ALL
       
 ------------------------------------------------------------------------WORKING WITH MULTIPLE TABLES
-____________________________________________________________________________________________________      
+____________________________________________________________________________________________________
+
+Ways to access multiple tables in the same query:
+- Sub-queries
+- Implicit JOIN
+- JOIN operators    (described along with other JOIN operators in relevant section)
+
+____________________________________________________________________________________________________
+SUB-QUERY
+
+SELECT DEPT_ID_DEP, DEP_NAME
+FROM DEPARTMENTS
+WHERE DEPT_ID_DEP IN (
+  SELECT DEP_ID
+  FROM EMPLOYEES
+  WHERE SALARY > 70000);
+  
+____________________________________________________________________________________________________
+IMPLICIT JOIN
+
+SELECT *
+FROM EMPLOYEES, DEPARTMENTS
+
+SELECT *
+FROM EMPLOYEES AS E, DEPARTMENTS AS D
+WHERE E.DEP_ID = D.DEPT_ID_DEP;
