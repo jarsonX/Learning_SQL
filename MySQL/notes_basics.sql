e0c88c8d72b86bf1015a3986c16cc661c5697062
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-10 13:01:18 +0200

    Update notes_basics.sql

--- MySQL/notes_basics.sql
+++ MySQL/notes_basics.sql
@@ -139,35 +139,3 @@ TRUNCATE - deletes all rows in a table
        TRUNCATE TABLE name
               IMMEDIATE     (optional)
   
-  
--------------------------------------------------------------------------SUB-QUERIES, NESTED QUERIES
-____________________________________________________________________________________________________
-
-Sub-queries can be used in:
-- WHERE clause
-- SELECT clause (list of columns)
-- FROM clause
-
-____________________________________________________________________________________________________
-in WHERE
-
-SELECT EMP_ID, F_NAME, L_NAME, SALARY
-FROM EMPLOYEES
-WHERE SALARY > (
-                SELECT AVG(SALARY)
-                FROM EMPLOYEES
-                );
-
-____________________________________________________________________________________________________
-in SELECT (column expressions)
-
-SELECT EMP_ID, SALARY, (SELECT AVG(SALARY)
-                        FROM EMPLOYEES) AS AVG_SALARY
-FROM EMPLOYEES
-
-____________________________________________________________________________________________________
-in FROM (derived tables/derived expressions)
-
-SELECT *
-FROM (SELECT EMP_ID, F_NAME, L_NAME, DEP_ID
-      FROM EMPLOYEES) AS EMP4ALL
