b176dd506b3bfaf687f7d993f4ed5dfa87bb9cf1
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-14 16:34:43 +0200

    Create notes_useful-functions.sql

---
+++ SQL Server/notes_useful-functions.sql
@@ -0,0 +1,11 @@
+____________________________________________________________________________________________________
+USEFUL FUNCTIONS
+
+LEN (string_expression)
+LEFT/RIGHT (text, length)
+SUBSTRING (text, start, length)
+LTRIM (text) - czyści białe znaki
+REPLACE(text, pattern, replacement)
+CONCAT(text1, text2, textN)
+LOWER/UPPER(text)
+CHARINDEX(text_to_find, text_to_search, [start])
