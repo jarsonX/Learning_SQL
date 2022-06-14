e53bca7bbddeb378d4651b4564889f6bd3fd0973
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-14 16:34:25 +0200

    Update notes_intermediate-in-polish.sql

--- SQL Server/notes_intermediate-in-polish.sql
+++ SQL Server/notes_intermediate-in-polish.sql
@@ -5,18 +5,6 @@ CAST
 
 CAST(expression AS data_type [(length)]) - konwertuje zmienną na inny typ
 
-_________________________________________________________________________________________________________________
-PRZYDATNE FUNKCJE
-
-LEN (string_expression)
-LEFT/RIGHT (text, length)
-SUBSTRING (text, start, length)
-LTRIM (text) - czyści białe znaki
-REPLACE(text, pattern, replacement)
-CONCAT(text1, text2, textN)
-LOWER/UPPER(text)
-CHARINDEX(text_to_find, text_to_search, [start])
-
 ________________________________________________________________________________________________________________
 ZMIENNE
 
