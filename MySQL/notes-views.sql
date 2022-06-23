9d4d5f11e9c6219ed3efbf36a7cece0960720166
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-23 11:52:46 +0200

    Create notes-views.sql

---
+++ MySQL/notes-views.sql
@@ -0,0 +1,11 @@
+CREATE VIEW view_name AS
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+
+CREATE OR REPLACE VIEW view_name AS
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;
+
+DROP VIEW view_name;
