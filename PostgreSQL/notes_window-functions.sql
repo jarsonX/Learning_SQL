374447384fafd7f28e53eef1c86009d4f7c9457c
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-06-30 13:10:26 +0200

    Update notes_window-functions.sql

--- PostgreSQL/notes_window-functions.sql
+++ PostgreSQL/notes_window-functions.sql
@@ -101,7 +101,7 @@ SELECT
       home_goal,
       away_goal,
       SUM(home_goal)
-            OVER(ORDER BY date ROWS BETWEEN
+            OVER(ORDER BY date DESC ROWS BETWEEN
                  UNBOUNDED PRECIDING AND CURRENT ROW) AS running_total
 FROM match
 WHERE home_team_id = 8456 AND season = '2011/2012';
