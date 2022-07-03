24cbdc9ca3c15016d7cc037147fb6584fa5b4b49
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-03 10:10:44 +0200

    Update notes_different-techniques.sql

--- PostgreSQL/notes_different-techniques.sql
+++ PostgreSQL/notes_different-techniques.sql
@@ -3,7 +3,7 @@ The same problem resolved with: subqueries, correlated subqueries and CTEs.
 ------------------------------------------------------------------------------------------SUBQUERIES
 ----------------------------------------------------------------------------------------------------
 SELECT
-	m.date,
+    m.date,
     -- Get the home and away team names
     home.hometeam,
     away.awayteam,
@@ -31,42 +31,44 @@ ON away.id = m.id
 -------------------------------------------------------------------------------CORRELATED-SUBQUERIES
 ----------------------------------------------------------------------------------------------------
 SELECT
-    m.date,
-    (SELECT team_long_name
+     m.date,
+     
+     (SELECT team_long_name
      FROM team AS t
      WHERE t.team_api_id = m.hometeam_id) AS hometeam,
-    -- Connect the team to the match table
-    (SELECT team_long_name
+
+     (SELECT team_long_name
      FROM team AS t
      WHERE t.team_api_id = m.awayteam_id) AS awayteam,
-    -- Select home and away goals
+
      home_goal,
      away_goal
+
 FROM match AS m;
 
 ------------------------------------------------------------------------------------------------CTEs
 ----------------------------------------------------------------------------------------------------
 WITH home AS (
   SELECT m.id, m.date, 
-  		 t.team_long_name AS hometeam, m.home_goal
+  	 t.team_long_name AS hometeam, m.home_goal
   FROM match AS m
   LEFT JOIN team AS t 
   ON m.hometeam_id = t.team_api_id),
--- Declare and set up the away CTE
+
 away AS (
   SELECT m.id, m.date, 
-  		 t.team_long_name AS awayteam, m.away_goal
+  	 t.team_long_name AS awayteam, m.away_goal
   FROM match AS m
   LEFT JOIN team AS t 
   ON m.awayteam_id = t.team_api_id)
--- Select date, home_goal, and away_goal
+
 SELECT 
-	home.date,
+    home.date,
     home.hometeam,
     away.awayteam,
     home.home_goal,
     away.away_goal
--- Join away and home on the id column
+
 FROM home
 INNER JOIN away
 ON home.id = away.id;
