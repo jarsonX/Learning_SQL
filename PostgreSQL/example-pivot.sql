7ba8034536382e339072560b608b7da22edeeef9
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-05 11:03:42 +0200

    Update example-pivot.sql

--- PostgreSQL/example-pivot.sql
+++ PostgreSQL/example-pivot.sql
@@ -1,6 +1,7 @@
-A basic pivot
+____________________________________________________________________________________________________
+BASIC PIVOT
 
-You have the following table of Pole Vault gold medalist countries by gender in 2008 and 2012.
+--You have the following table of Pole Vault gold medalist countries by gender in 2008 and 2012.
 
 | Gender | Year | Country |
 |--------|------|---------|
@@ -9,15 +10,13 @@ You have the following table of Pole Vault gold medalist countries by gender in 
 | Women  | 2008 | RUS     |
 | Women  | 2012 | USA     |
 
-Pivot it by Year to get the following reshaped, cleaner table.
+--Pivot it by Year to get the following reshaped, cleaner table.
 
 | Gender | 2008 | 2012 |
 |--------|------|------|
 | Men    | AUS  | FRA  |
 | Women  | RUS  | USA  |
 
-
---Solution
 
 -- Create the correct extention to enable CROSSTAB
 CREATE EXTENSION IF NOT EXISTS tablefunc;
@@ -37,3 +36,60 @@ $$) AS ct (Gender VARCHAR,
            "2012" VARCHAR)
 
 ORDER BY Gender ASC;
+
+____________________________________________________________________________________________________
+PIVOTING WITH RANKING
+
+--You want to produce an easy scannable table of the rankings of the three most populous EU countries 
+--by how many gold medals they've earned in the 2004 through 2012 Olympic games. The table needs to 
+--be in this format:
+
+| Country | 2004 | 2008 | 2012 |
+|---------|------|------|------|
+| FRA     | ...  | ...  | ...  |
+| GBR     | ...  | ...  | ...  |
+| GER     | ...  | ...  | ...  |
+
+--You'll need to count the gold medals each country has earned, produce the ranks of each country by 
+--medals earned, then pivot the table to this shape.
+
+--STEP-1
+--Count the gold medals that France (FRA), the UK (GBR), and Germany (GER) have earned per country 
+--and year.
+
+--STEP-2
+--Select the country and year columns, then rank the three countries by how many gold medals they 
+--earned per year.
+
+--STEP-3
+--Pivot the query's results by Year by filling in the new table's correct column names.
+
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  WITH Country_Awards AS (
+    SELECT
+      Country,
+      Year,
+      COUNT(*) AS Awards
+    FROM Summer_Medals
+    WHERE
+      Country IN ('FRA', 'GBR', 'GER')
+      AND Year IN (2004, 2008, 2012)
+      AND Medal = 'Gold'
+    GROUP BY Country, Year)
+
+  SELECT
+    Country,
+    Year,
+    RANK() OVER
+      (PARTITION BY Year
+       ORDER BY Awards DESC) :: INTEGER AS rank
+  FROM Country_Awards
+  ORDER BY Country ASC, Year ASC;
+$$) AS ct (Country VARCHAR,
+           "2004" INTEGER,
+           "2008" INTEGER,
+           "2012" INTEGER)
+
+Order by Country ASC;
