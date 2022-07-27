c92e3deb9db23d3dda09ea621951d5582b95a30b
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-27 17:34:03 +0200

    Update notes_aggregating-time-series.sql

--- SQL Server/notes_aggregating-time-series.sql
+++ SQL Server/notes_aggregating-time-series.sql
@@ -1,4 +1,4 @@
---KEY-AGGREGATION-FUNCTIONS-------------------------------------------------------------------------
+--BASIC-AGGREGATION-FUNCTIONS-----------------------------------------------------------------------
 ____________________________________________________________________________________________________
 
 COUNT()
@@ -16,6 +16,25 @@ COUNT(Column)  --returns number of rows in Column where the value IS NOT NULL
 COUNT(NULLIF(Column, 1990))  --set NULL if 1990 and count NOT NULL values in the column
 
 
+--STATISTICAL-AGGREGATION-FUNCTIONS-----------------------------------------------------------------
+____________________________________________________________________________________________________
+
+AVG()
+STDEV()  --standard deviation
+STDEVP()  --population standard deviation (only if we're looking at the entire population)
+VAR()  --variance
+VARP()  --population variance
+
+--There's no median function built-in.
+
+PERCENTILE_CONT() --takes a parameter, which is percentile you'd like, e.g. we want 50th percentile
+--or 'median'.
+
+SELECT TOP(1)
+  PERCENTILE_CONT(0.5)
+    WITHIN GROUP(ORDER BY SomeValue DESC)
+    OVER() AS Median
+FROM SomeTable    
 
 
 
