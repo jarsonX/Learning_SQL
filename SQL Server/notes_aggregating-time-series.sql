c97fdab5f86b9b829c6f200c73de1d1af09a6201
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-07-27 17:08:40 +0200

    Update notes_aggregating-time-series.sql

--- SQL Server/notes_aggregating-time-series.sql
+++ SQL Server/notes_aggregating-time-series.sql
@@ -1,1 +1,23 @@
+--KEY-AGGREGATION-FUNCTIONS-------------------------------------------------------------------------
+____________________________________________________________________________________________________
+
+COUNT()
+COUNT_BIG()           --like COUNT() but returns BIGINT (64-bit INTEGER)
+COUNT(DISTINCT ...)
+SUM()
+MIN()
+MAX()
+
+--What-counts-with-COUNT----------------------------------------------------------------------------
+
+COUNT(*)  --returns number of rows, including nulls
+COUNT(Column)  --returns number of rows in Column where the value IS NOT NULL
+
+COUNT(NULLIF(Column, 1990))  --set NULL if 1990 and count NOT NULL values in the column
+
+
+
+
+
+
 
