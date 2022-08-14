a715ca17e27a91e52e6db8c920949a7260666a99
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-08-14 15:47:56 +0200

    Create notes_dates-manipulating_time.sql

---
+++ SQL Server/notes_dates-manipulating_time.sql
@@ -0,0 +1,9 @@
+--MANIPULATING-TIME-----------------------------------------------------------------------
+__________________________________________________________________________________________
+
+--Function that return system date and time
+
+--Higher precision                    --Lower precision
+SYSDATETIME()                         GETDATE()
+SYSUTCDATETIME()                      GETUTCDATE()
+SYSDATETIMEOFFSET()                   CURRENT_TIMESTAMP
