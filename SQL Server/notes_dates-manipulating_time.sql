fcf2b54eb58295516c2b454fc52d1cdf67d4be3d
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-08-14 15:52:31 +0200

    Update notes_dates-manipulating_time.sql

--- SQL Server/notes_dates-manipulating_time.sql
+++ SQL Server/notes_dates-manipulating_time.sql
@@ -11,3 +11,19 @@ SYSDATETIMEOFFSET()                   CURRENT_TIMESTAMP
 
 --RETURNING-DATE-AND-TIME-PARTS-----------------------------------------------------------
 __________________________________________________________________________________________
+
+YEAR()
+MONTH()
+DAY()
+
+--DATENAME--------------------------------------------------------------------------------
+
+--Returns a certain part of the date.
+
+DATENAME(datepart, date)
+
+year        yyyy, yy
+month       mm, m
+dayofyear   dy, y
+week        wk, ww
+weekday     dw, w
