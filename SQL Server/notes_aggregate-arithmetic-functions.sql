33a37f8926d04223b80a501e8bc45bb5e209a8ee
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-08-22 14:16:28 +0200

    Update notes_aggregate-arithmetic-functions.sql

--- SQL Server/notes_aggregate-arithmetic-functions.sql
+++ SQL Server/notes_aggregate-arithmetic-functions.sql
@@ -1,6 +1,12 @@
 ____________________________________________________________________________________________________
 --ANALYTIC-FUNCTIONS--------------------------------------------------------------------------------
 
+--Calculate aggregate value on a group of rows. They are computed on each row, instead of a group of
+--data.
+
 FIRST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY COLUMN ROW_or_RANGE frame)
 --Returns the first value in an ordered set. ROW_or_RANGE frame sets the partition limits.
+
+LAST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY column ROW_or_RANGE frame)
+--Returns the last value in an ordered set.
 
