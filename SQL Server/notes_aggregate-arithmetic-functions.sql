557811663e0e8144c066c28da286b10131075d4e
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-08-22 14:12:17 +0200

    Update notes_aggregate-arithmetic-functions.sql

--- SQL Server/notes_aggregate-arithmetic-functions.sql
+++ SQL Server/notes_aggregate-arithmetic-functions.sql
@@ -1,2 +1,6 @@
 ____________________________________________________________________________________________________
-----------------------------------------------------------------------------------------------------
+--ANALYTIC-FUNCTIONS--------------------------------------------------------------------------------
+
+FIRST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY COLUMN ROW_or_RANGE frame)
+--Returns the first value in an ordered set. ROW_or_RANGE frame sets the partition limits.
+
