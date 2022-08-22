96be0329ab8373a1008c5ef0f5d919ab75309fc2
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-08-22 14:21:16 +0200

    Update notes_aggregate-arithmetic-functions.sql

--- SQL Server/notes_aggregate-arithmetic-functions.sql
+++ SQL Server/notes_aggregate-arithmetic-functions.sql
@@ -10,3 +10,18 @@ FIRST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY COLUMN ROW_o
 LAST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY column ROW_or_RANGE frame)
 --Returns the last value in an ordered set.
 
+ROW_or_RANGE frame
+
+RANGE BETWEEN start_boundary AND end_boundary
+ROWS BETWEEN start_boundary AND end_boundary
+
+--|---------------------|----------------------------|
+--| Boundary            | Description                |
+--|---------------------|----------------------------|
+--| UNBOUNDED PRECEDING | first row in the partition |
+--| UNBOUNDED FOLLOWING | last row in the partition  |
+--| CURRENT ROW         | current row                |
+--| PRECEDING           | previous row               |
+--| FOLLOWING           | next row                   |
+--|---------------------|----------------------------|
+
