7ff49300d8104e1071e056c876db2a4518d9c10c
Jarek <105446316+jarsonX@users.noreply.github.com>
2022-08-22 14:28:32 +0200

    Update notes_aggregate-arithmetic-functions.sql

--- SQL Server/notes_aggregate-arithmetic-functions.sql
+++ SQL Server/notes_aggregate-arithmetic-functions.sql
@@ -10,8 +10,10 @@ FIRST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY COLUMN ROW_o
 LAST_VALUE(numeric_expression) OVER([PARTITION BY column] ORDER BY column ROW_or_RANGE frame)
 --Returns the last value in an ordered set.
 
-ROW_or_RANGE frame
+PARTITION LIMITS
 --The limits of each partition can be explicitly specified after ORDER BY from the OVER clause.
+--Remember that analytic functions are applied by default from the first row of the partition
+--until the current row.
 
 RANGE BETWEEN start_boundary AND end_boundary
 ROWS BETWEEN start_boundary AND end_boundary
@@ -26,3 +28,17 @@ ROWS BETWEEN start_boundary AND end_boundary
 --| FOLLOWING           | next row                   |
 --|---------------------|----------------------------|
 
+EXAMPLE
+--You want to compare the total votes of each person with the minimum number of votes recorded by
+--a person and with the maximum. The voters should be divided per gender.
+
+SELECT
+    first_name + ' ' + last_name AS name,
+    gender,
+    total_votes AS votes,
+    FIRST_VALUE(total_votes)
+        OVER(PARTITION BY gender ORDER BY total_votes) AS min_votes,
+    LAST VALUE(total_votes)
+        OVER(PARTITION BY gender ORDER BY total_votes
+             ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS max_votes
+FROM voters             
